b650673c760ba2c429452c4bc5d45119d4dde3a0
Alasdair Law <alasdair.law@icloud.com>
2025-03-23 15:38:52 +0000

    Install the fork code

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.
 
 # Config
 echo "Cloning Config"
-git clone https://github.com/TheGoldenPatrik1/sketchybar-config /tmp/sketchybar
+git clone https://github.com/alasdairlaw/sketchybar-config /tmp/sketchybar
 mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
 mv /tmp/sketchybar $HOME/.config/sketchybar
 chmod +x $HOME/.config/sketchybar/scripts/*.sh
